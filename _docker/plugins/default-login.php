111e55b5e6fb079ba626762c6fd15e34f92946a6
Jack Ellis <jackellis1504@gmail.com>
2024-11-06 19:30:13 +0000

    docker and some viewing updates
     M .env
     D compose.override.yaml
     M compose.yaml
     M src/Framework/Controller/CharacterController.php
     M templates/character/view.html.twig
    ?? _docker/

---
+++ _docker/plugins/default-login.php
@@ -0,0 +1,24 @@
+<?php
+
+class DefaultLogin
+{
+    /** Print login form.
+     */
+    public function loginForm()
+    {
+        echo <<<'HTML'
+<input name="auth[driver]" value="sqlite" />
+<input name="auth[db]" value="data.db" />
+<input name="auth[permanent]" type="checkbox" value="1" checked />
+<input type="submit" value="Log In">
+HTML;
+
+        return 0;
+    }
+
+    public function login() {
+        return true;
+    }
+}
+
+return new DefaultLogin();
--- src/Framework/Controller/CharacterController.php
+++ src/Framework/Controller/CharacterController.php
@@ -14,7 +14,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route(path: '/character', name: 'character.')]
+#[Route(path: '', name: 'character.')]
 class CharacterController extends AbstractController
 {
     public function __construct(
