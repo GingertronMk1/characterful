19a7298d0f54b98f46ff3f9ad2c6ff57ea159763
Jack Ellis <jack@apex8.com>
2024-11-11 14:42:15 +0000

    update to tests with routing
     M tests/Application/CharacterControllerTest.php

--- tests/Application/CharacterControllerTest.php
+++ tests/Application/CharacterControllerTest.php
@@ -3,23 +3,30 @@
 namespace App\Tests\Application;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpKernel\HttpKernelBrowser;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class CharacterControllerTest extends WebTestCase
 {
+    private UrlGeneratorInterface $urlGenerator;
+    private HttpKernelBrowser $client;
+    public function setUp(): void
+    {
+        $this->client = static::createClient();
+        $container = static::getContainer();
+        $this->urlGenerator = $container->get(UrlGeneratorInterface::class);
+    }
+
     public function testIndex(): void
     {
-        $client = static::createClient();
-
-        $crawler = $client->request('GET', '/');
+        $crawler = $this->client->request('GET', $this->urlGenerator->generate('character.index'));
 
         $this->assertResponseIsSuccessful();
     }
 
     public function testCreate(): void
     {
-        $client = static::createClient();
-
-        $crawler = $client->request('GET', '/create');
+        $crawler = $this->client->request('GET', $this->urlGenerator->generate('character.create'));
 
         $this->assertResponseIsSuccessful();
     }
